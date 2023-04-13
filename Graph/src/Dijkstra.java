6374cd7f6d63df9654a8e874f81ad8a88b5b3b6e
J.B. Neto <netobatista090@gmail.com>
2023-04-12 23:41:55 -0300

    Melhoria do código de Dikstra e refatoramento do mesmo. Correção do bug do ponto solto.

--- Graph/src/Dijkstra.java
+++ Graph/src/Dijkstra.java
@@ -1,76 +1,92 @@
-import java.awt.*;
-import java.util.*;
+import java.awt.Point;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Dijkstra {
+    private List<Route> routesList;
+    private List<Point> verticesList;
 
-    public static List<Integer> lowestCostRoute(List<Route> routes, Point origin, Point destination) {
-
-        Map<Point, Integer> distance = new HashMap<>();
-        Map<Point, Point> prev = new HashMap<>();
-        Set<Point> unvisited = new HashSet<>();
-
-        // inicializa as distâncias e os predecessores
-        for (Route route : routes) {
-            distance.put(route.getOrigin(), Integer.MAX_VALUE);
-            distance.put(route.getDestiny(), Integer.MAX_VALUE);
-            prev.put(route.getOrigin(), null);
-            prev.put(route.getDestiny(), null);
-            unvisited.add(route.getOrigin());
-            unvisited.add(route.getDestiny());
-        }
-
-        distance.put(origin, 0);
-
-        while (!unvisited.isEmpty()) {
-            // encontra o vértice com a menor distância
-            Point current = null;
-            int minDistance = Integer.MAX_VALUE;
-            for (Point point : unvisited) {
-                int dist = distance.get(point);
-                if (dist < minDistance) {
-                    minDistance = dist;
-                    current = point;
-                }
-            }
-
-            unvisited.remove(current);
-
-            // calcula a distância dos vértices vizinhos
-            for (Route route : routes) {
-                if (route.getOrigin().equals(current)) {
-                    int alt = distance.get(current) + route.getWeight();
-                    if (alt < distance.get(route.getDestiny())) {
-                        distance.put(route.getDestiny(), alt);
-                        prev.put(route.getDestiny(), current);
-                    }
-                } else if (route.getDestiny().equals(current)) {
-                    int alt = distance.get(current) + route.getWeight();
-                    if (alt < distance.get(route.getOrigin())) {
-                        distance.put(route.getOrigin(), alt);
-                        prev.put(route.getOrigin(), current);
-                    }
-                }
-            }
-        }
-
-        // constrói a menor rota
-        List<Integer> shortestPath = new ArrayList<>();
-        Point current = destination;
-        while (prev.get(current) != null) {
-            for (int i = 0; i < routes.size(); i++) {
-                Route route = routes.get(i);
-                if ((route.getOrigin().equals(prev.get(current)) && route.getDestiny().equals(current))
-                        || (route.getDestiny().equals(prev.get(current)) && route.getOrigin().equals(current))) {
-                    shortestPath.add(i);
-                    break;
-                }
-            }
-            current = prev.get(current);
-        }
-
-        Collections.reverse(shortestPath);
-
-        return shortestPath;
+    public Dijkstra(List<Route> routesList, List<Point> verticesList) {
+        this.routesList = routesList;
+        this.verticesList = verticesList;
     }
+
+    public List<Integer> lowestCostRoute(Point origin, Point destiny) {
+        int numVertices = verticesList.size();
+        List<List<Route>> graph = new ArrayList<>();
+        for (int i = 0; i < numVertices; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (Route route : routesList) {
+            int indexOrigin = verticesList.indexOf(route.getOrigin());
+            int indexDestiny = verticesList.indexOf(route.getDestiny());
+            graph.get(indexOrigin).add(route);
+            graph.get(indexDestiny).add(route);
+        }
+
+        int startIndex = verticesList.indexOf(origin);
+        int endIndex = verticesList.indexOf(destiny);
+
+        int[] distances = new int[numVertices];
+        int[] previousVertices = new int[numVertices];
+        PriorityQueue<Integer> pq = new PriorityQueue<>((u, v) -> distances[u] - distances[v]);
+        for (int i = 0; i < numVertices; i++) {
+            if (i == startIndex) {
+                distances[i] = 0;
+            } else {
+                distances[i] = Integer.MAX_VALUE;
+            }
+            previousVertices[i] = -1;
+            pq.add(i);
+        }
+
+        while (!pq.isEmpty()) {
+            int u = pq.poll();
+            if (distances[u] == Integer.MAX_VALUE) {
+                break;
+            }
+
+            for (Route route : graph.get(u)) {
+                int v = verticesList.indexOf(getOtherVertex(u, route));
+                int alternativeDistance = distances[u] + route.getWeight();
+                if (alternativeDistance < distances[v]) {
+                    distances[v] = alternativeDistance;
+                    previousVertices[v] = u;
+                    pq.remove(v);
+                    pq.add(v);
+                }
+            }
+        }
+
+        List<Integer> path = new ArrayList<>();
+        int u = endIndex;
+        while (previousVertices[u] != -1) {
+            path.add(0, getRouteIndex(previousVertices[u], u));
+            u = previousVertices[u];
+        }
+
+        return path;
+    }
+
+    private Point getOtherVertex(int u, Route route) {
+        if (route.getOrigin().equals(verticesList.get(u))) {
+            return route.getDestiny();
+        } else {
+            return route.getOrigin();
+        }
+    }
+
+    private int getRouteIndex(int u, int v) {
+        for (int i = 0; i < routesList.size(); i++) {
+            Route route = routesList.get(i);
+            if ((route.getOrigin().equals(verticesList.get(u)) && route.getDestiny().equals(verticesList.get(v))) ||
+                    (route.getOrigin().equals(verticesList.get(v)) && route.getDestiny().equals(verticesList.get(u)))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
